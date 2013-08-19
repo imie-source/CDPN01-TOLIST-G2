1fab4bb1d421687f8d8659011458849a63f688f7
Kevin <k.thedrez@hotmail.fr>
2013-08-19 16:52:01 +0200

    ++ logo

--- js/main.js
+++ js/main.js
@@ -13,6 +13,8 @@ var App = {
     var el = document.getElementById("logo");
     var ctx = el.getContext("2d");
     ctx.beginPath();
+
+    // I
     ctx.moveTo(10,13);
     ctx.lineTo(30,13);
     ctx.moveTo(20,13);
@@ -21,6 +23,7 @@ var App = {
     ctx.lineTo(30,77);
 
 
+    // M
     ctx.moveTo(40,80);
     ctx.lineTo(40,10);
     ctx.lineTo(50,50);
@@ -28,6 +31,7 @@ var App = {
     ctx.lineTo(60,80);
 
 
+    // I
     ctx.moveTo(70,13);
     ctx.lineTo(90,13);
     ctx.moveTo(80,13);
@@ -36,6 +40,7 @@ var App = {
     ctx.lineTo(90,77);
 
 
+    // E
     ctx.moveTo(130,13);
     ctx.lineTo(100,13);
     ctx.lineTo(100,77);
@@ -47,5 +52,37 @@ var App = {
     ctx.lineWidth = 8;
     ctx.strokeStyle = "#321564";
     ctx.stroke();
+
+    // Smiley :)
+
+	// Visage
+	ctx.beginPath();
+	ctx.arc(200,50,40,0,Math.PI*2,true);
+	ctx.strokeStyle = "#333";
+	ctx.lineWidth = 1;
+	ctx.fillStyle = "yellow";
+	ctx.fill();
+	ctx.stroke();
+
+	// Bouche
+	ctx.beginPath();
+	ctx.lineWidth = 1;
+	ctx.arc(200,60,20,0,Math.PI,false);
+	ctx.strokeStyle = "#333";
+	ctx.fillStyle = "red";
+	ctx.lineCap = "round";
+	ctx.fill();
+
+	// Yeux
+	ctx.beginPath();
+	ctx.strokeStyle = "#369";
+	ctx.fillStyle="red";
+	ctx.arc(230,20,10,0,Math.PI*2,false);
+	ctx.fill();
+	ctx.stroke();
+	ctx.beginPath();
+	ctx.arc(170,20,10,0,Math.PI*2,false); 
+	ctx.stroke();
+	ctx.fill();
   }
 };
