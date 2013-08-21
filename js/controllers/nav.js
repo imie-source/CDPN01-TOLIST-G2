aabfeaeeecb915ea3014f302c4cc82f44049d686
fabienroue <roue.fabien@gmail.com>
2013-08-21 09:53:03 +0200

    test

--- js/controllers/nav.js
+++ js/controllers/nav.js
@@ -11,27 +11,10 @@ App.controllers.Nav = Backbone.Router.extend({
   },
 
   showContact: function(id){
-<<<<<<< HEAD
-  	console.log("Contactttt");
-  	console.log(id);
-	
-	$('input[name=firstName]').val() = $('table').rows[id].cells[0];
-	$('input[name=lastName]').val() = $('table').rows[id].cells[1];
-	$('input[name=phoneNumber]').val() = $('table').rows[id].cells[2];
-	$('input[name=birthday]').val() = $('table').rows[id].cells[3];
-	form.style.visibility = "visible";	
-  },
-
-  newContact: function() {
-  	console.log("helloooo");
-	new App.views.Form();
-	document.getElementById("userForm").style.visibility = "visible";
-=======
     App.changePage("form");
   },
 
   newContact: function() {
     App.changePage("form");
->>>>>>> 77bd1517b28722ff9bc55f94ccb459eacdfb4888
   }
 });
--- js/main.js
+++ js/main.js
@@ -6,13 +6,7 @@ var App = {
 
   init: function() {
     new App.controllers.Nav();
-<<<<<<< HEAD
-    new App.views.Home();
-    Backbone.history.start();	
-	document.getElementById("userForm").style.visibility = "hidden";
-=======
     Backbone.history.start();
->>>>>>> 77bd1517b28722ff9bc55f94ccb459eacdfb4888
   },
 
   changePage: function(view) {
@@ -21,13 +15,9 @@ var App = {
   }
 };
 
-<<<<<<< HEAD
-
-=======
 Backbone.sync = function(method, model) {
   (new App.collections.Users()).fetch({ success: function(users) {
     users.add(model);
     localStorage.setItem("users", JSON.stringify(users));
   }});
 }
->>>>>>> 77bd1517b28722ff9bc55f94ccb459eacdfb4888
